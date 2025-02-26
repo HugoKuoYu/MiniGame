30a3fdcf9818e378e81b0608e893bc4c81111323
hugo_kuo <hugo_kuo@9splay.com>
2025-02-26 16:47:01 +0800

    新增addressable、uibase，移動firebase位置

---
+++ MiniGame/Assets/Scripts/UI/UIBase.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIBase : MonoBehaviour
+{
+
+    public CanvasGroup canvasGroup;
+    public virtual void Initialize()
+    { 
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+    public virtual void Hide()
+    {
+        if (canvasGroup != null)
+        {
+            canvasGroup.alpha = 0;//畫布隱形
+            canvasGroup.interactable = false;//不可互動
+            canvasGroup.blocksRaycasts = false;//關閉雷射
+        }
+        else//若畫布為null
+        { 
+            gameObject.SetActive(false);//直接禁用
+        }
+    }
+    public virtual void Show()
+    {
+        if (canvasGroup != null)
+        {
+            canvasGroup.alpha = 1;//畫布顯示
+            canvasGroup.interactable = true;//可互動
+            canvasGroup.blocksRaycasts = true;//雷射開啟
+        }
+        else//若畫布為null
+        {
+            gameObject.SetActive(true);//直接顯示物件
+        }
+    }
+    public virtual void Close()
+    {
+        Hide();
+        Destroy(gameObject);
+    }
+}
